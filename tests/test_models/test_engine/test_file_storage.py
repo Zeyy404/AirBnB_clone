ea3cd9b315aa2ef95148dbd366c504dfde51f8ff
Zainab Salih <Zainab.Salih@outlook.com>
2024-02-14 02:35:50 +0000

    Test Suite

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,6 +2,7 @@
 """unit tests for the file_storage module"""
 import unittest
 import os
+from io import StringIO
 import json
 import sys
 sys.path.append('../../')
@@ -49,6 +50,20 @@ class TestFileStorage(unittest.TestCase):
         storage_all = self.storage.all()
         self.assertIsInstance(storage_all, dict)
 
+    def test_save(self, mock_json_dump, mock_open):
+        """File_storage"""
+        self.storage.new(self.base_model)
+        self.storage.save()
+        mock_open.assert_called_once_with((FileStorage._FileStorage__file_path,
+                                           "w", encoding="utf-8"))
+        mock_json_dump.assert_called_once()
+
+    def test_all(self):
+        """File_storage"""
+        self.storage.new(self.base_model)
+        all_objects = self.storage.all()
+        self.assertEqual(len(all_objects), 1)
+
     def test_new_method(self):
         """Tests that the new method sets the right key and value pair
             in the FileStorage.__object attribute.
@@ -57,6 +72,29 @@ class TestFileStorage(unittest.TestCase):
         self.storage.new(self.my_model)
         key = str(self.my_model.__class__.__name__ + "." + self.my_model.id)
         self.assertTrue(key in self.storage._FileStorage__objects)
+
+    def test_reload_empty(self, mock_open, mock_stat):
+        """File_storage"""
+        mock_stat.return_value.st_size = 0
+        self.storage.reload()
+        self.assertFalse(self.storage._FileStorage__objects)
+
+    def test_reload_with_data(self, mock_open):
+        """File_storage"""
+        self.storage.reload()
+        self.assertTrue(self.storage._FileStorage__objects)
+        key = "BaseModel.1234"
+        self.assertIn(key, self.storage._FileStorage__objects)
+        obj = self.storage._FileStorage__objects[key]
+        self.assertIsInstance(obj, BaseModel)
+        self.assertEqual(obj.id, "1234")
+
+     def test_reload_not_found(self, mock_open):
+         """File_storage"""
+         self.storage.reload()
+         mock_open.assert_called_once_with((FileStorage._FileStorage__file_path,
+                                            "r+", encoding="utf-8"))
+         self.assertFalse(self.storage._FileStorage__objects)
 
     def test_objects_value_type(self):
         """Tests that the type of value contained in the FileStorage.__object
