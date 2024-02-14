c945b6c7158589c709969f86c3c769830052b197
Zainab Salih <Zainab.Salih@outlook.com>
2024-02-14 02:44:54 +0000

    Test Suite

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -19,14 +19,6 @@ class TestFileStorage(unittest.TestCase):
         self.my_model = BaseModel()
         self.storage = FileStorage()
 
-    def tearDown(self):
-        """Cleans up after testing is done"""
-
-        try:
-            os.remove("file.json")
-        except FileNotFoundError:
-            pass
-
     def test_FileStorage_instializing(self):
         """Test initializing with no args"""
 
@@ -50,19 +42,6 @@ class TestFileStorage(unittest.TestCase):
         storage_all = self.storage.all()
         self.assertIsInstance(storage_all, dict)
 
-    def test_save(self, mock_json_dump, mock_open):
-        """File_storage"""
-        self.storage.new(self.base_model)
-        self.storage.save()
-        mock_open.assert_called_once_with((FileStorage._FileStorage__file_path,
-                                           "w", encoding="utf-8"))
-        mock_json_dump.assert_called_once()
-
-    def test_all(self):
-        """File_storage"""
-        self.storage.new(self.base_model)
-        all_objects = self.storage.all()
-        self.assertEqual(len(all_objects), 1)
 
     def test_new_method(self):
         """Tests that the new method sets the right key and value pair
@@ -72,29 +51,6 @@ class TestFileStorage(unittest.TestCase):
         self.storage.new(self.my_model)
         key = str(self.my_model.__class__.__name__ + "." + self.my_model.id)
         self.assertTrue(key in self.storage._FileStorage__objects)
-
-    def test_reload_empty(self, mock_open, mock_stat):
-        """File_storage"""
-        mock_stat.return_value.st_size = 0
-        self.storage.reload()
-        self.assertFalse(self.storage._FileStorage__objects)
-
-    def test_reload_with_data(self, mock_open):
-        """File_storage"""
-        self.storage.reload()
-        self.assertTrue(self.storage._FileStorage__objects)
-        key = "BaseModel.1234"
-        self.assertIn(key, self.storage._FileStorage__objects)
-        obj = self.storage._FileStorage__objects[key]
-        self.assertIsInstance(obj, BaseModel)
-        self.assertEqual(obj.id, "1234")
-
-     def test_reload_not_found(self, mock_open):
-         """File_storage"""
-         self.storage.reload()
-         mock_open.assert_called_once_with((FileStorage._FileStorage__file_path,
-                                            "r+", encoding="utf-8"))
-         self.assertFalse(self.storage._FileStorage__objects)
 
     def test_objects_value_type(self):
         """Tests that the type of value contained in the FileStorage.__object
@@ -111,3 +67,11 @@ class TestFileStorage(unittest.TestCase):
 
         self.storage.save()
         self.assertTrue(os.path.isfile("file.json"))
+
+    def tearDown(self):
+        """Cleans up after testing is done"""
+
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
